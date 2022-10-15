3758dce4ef1934aa769a1868ddd062023eb0be20
mubarak_ansari <mubarak.ansari@brainvire.com>
2022-10-15 19:17:46 +0530

    Update api calling

---
+++ app/src/main/java/com/mubarak/android_degger_hilt/utils/PrefKey.kt
@@ -0,0 +1,13 @@
+package com.mubarak.android_degger_hilt.utils
+
+
+/**
+ * PrefKeys Name
+ */
+object PrefKey {
+
+//    const val PREFERENCE_NAME = BuildConfig.APPLICATION_ID
+
+    const val ISLOGGEDIN = "isUserLoggedIn"
+
+}
