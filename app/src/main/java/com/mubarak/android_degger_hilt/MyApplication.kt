029daab127034bf671a2ac3e47f4b2023bca3462
mubarak_ansari <mubarak.ansari@brainvire.com>
2022-10-15 23:20:23 +0530

    Added Shared pref

--- app/src/main/java/com/mubarak/android_degger_hilt/MyApplication.kt
+++ app/src/main/java/com/mubarak/android_degger_hilt/MyApplication.kt
@@ -1,9 +1,25 @@
 package com.mubarak.android_degger_hilt
 
 import android.app.Application
+import com.mubarak.android_degger_hilt.utils.MyPreference
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 
 @HiltAndroidApp
 class MyApplication:Application() {
+
+    @Inject
+    lateinit var mPref: MyPreference
+
+    companion object {
+        private var instance: MyApplication? = null
+        fun applicationContext(): MyApplication {
+            return instance as MyApplication
+        }
+    }
+
+    init {
+        instance = this
+    }
 }
--- app/src/main/java/com/mubarak/android_degger_hilt/utils/PrefKey.kt
+++ app/src/main/java/com/mubarak/android_degger_hilt/utils/PrefKey.kt
@@ -1,12 +1,14 @@
 package com.mubarak.android_degger_hilt.utils
 
+import com.mubarak.android_degger_hilt.BuildConfig
+
 
 /**
  * PrefKeys Name
  */
 object PrefKey {
 
-//    const val PREFERENCE_NAME = BuildConfig.APPLICATION_ID
+   const val PREFERENCE_NAME = BuildConfig.APPLICATION_ID
 
     const val ISLOGGEDIN = "isUserLoggedIn"
 
